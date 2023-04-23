b6f3167f4d04c78cde2fcadef2a8b136f58ba523
she <fangfangshe09@gmail.com>
2023-04-24 08:46:15 +0900

    最寄り駅の実装終わりました

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,28 +1,27 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: %i[ show edit update destroy ]
 
-  # GET /properties or /properties.json
   def index
     @properties = Property.all
   end
 
-  # GET /properties/1 or /properties/1.json
   def show
+    # @property.nearest_stations.build
   end
 
-  # GET /properties/new
   def new
     @property = Property.new
+    @property.nearest_stations.new
   end
 
-  # GET /properties/1/edit
   def edit
+    @property.nearest_stations.new
+    # @property.nearest_stations.build
   end
 
-  # POST /properties or /properties.json
   def create
     @property = Property.new(property_params)
-
+    # @nearest_station = @nearest_stations.create(nearest_station_params)
     respond_to do |format|
       if @property.save
         format.html { redirect_to @property, notice: "Property was successfully created." }
@@ -34,7 +33,6 @@ class PropertiesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /properties/1 or /properties/1.json
   def update
     respond_to do |format|
       if @property.update(property_params)
@@ -47,7 +45,6 @@ class PropertiesController < ApplicationController
     end
   end
 
-  # DELETE /properties/1 or /properties/1.json
   def destroy
     @property.destroy
     respond_to do |format|
@@ -57,13 +54,12 @@ class PropertiesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_property
       @property = Property.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def property_params
-      params.require(:property).permit(:property, :rent, :address, :building_age, :remarks)
+      params.require(:property).permit(:property, :rent, :address, :building_age, :remarks,
+         nearest_stations_attributes: [:railway, :station_name, :walk_minutes, :id, :_destroy])
     end
 end
--- app/models/nearest_station.rb
+++ app/models/nearest_station.rb
@@ -1,3 +1,5 @@
 class NearestStation < ApplicationRecord
   belongs_to :property
+  # has_one :property
+  # accepts_nested_attributes_for :properties
 end
